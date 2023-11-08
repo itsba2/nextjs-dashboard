8334223fe035960f8b4bde74dc05bd0cf30dd808
Said Batuhan Bilmez <batuhan.bilmez@tum.de>
2023-11-08 20:55:08 +0100

    search and pagination of invoices

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -107,8 +107,6 @@ export async function fetchFilteredInvoices(
   query: string,
   currentPage: number,
 ) {
-  noStore();
-
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
 
   try {
@@ -141,8 +139,6 @@ export async function fetchFilteredInvoices(
 }
 
 export async function fetchInvoicesPages(query: string) {
-  noStore();
-
   try {
     const count = await sql`SELECT COUNT(*)
     FROM invoices
@@ -164,8 +160,6 @@ export async function fetchInvoicesPages(query: string) {
 }
 
 export async function fetchInvoiceById(id: string) {
-  noStore();
-
   try {
     const data = await sql<InvoiceForm>`
       SELECT
@@ -190,8 +184,6 @@ export async function fetchInvoiceById(id: string) {
 }
 
 export async function fetchCustomers() {
-  noStore();
-
   try {
     const data = await sql<CustomerField>`
       SELECT
@@ -210,8 +202,6 @@ export async function fetchCustomers() {
 }
 
 export async function fetchFilteredCustomers(query: string) {
-  noStore();
-
   try {
     const data = await sql<CustomersTable>`
 		SELECT
@@ -245,8 +235,6 @@ export async function fetchFilteredCustomers(query: string) {
 }
 
 export async function getUser(email: string) {
-  noStore();
-
   try {
     const user = await sql`SELECT * from USERS where email=${email}`;
     return user.rows[0] as User;
